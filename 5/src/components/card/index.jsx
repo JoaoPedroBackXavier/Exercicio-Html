4960fbb6170207d2a0a5e98b9f6a1b6f3d914a5f
JoaoPedroBackXavier <joaopedro.backxavier@gmail.com>
2023-10-31 20:20:17 -0300

    31/10, React JS

---
+++ 5/src/components/card/index.jsx
@@ -0,0 +1,7 @@
+import "./index.css"
+
+const MaisUmTexto = () => {
+    return "texto2"
+}
+
+export default MaisUmTexto;
---
+++ 5/src/pages/home/index.jsx
@@ -0,0 +1,12 @@
+import "./index.css"
+import MaisUmTexto from "../../components/card";
+
+const home = () => {
+    let textinho = MaisUmTexto
+    return <div>
+        <h1>texto</h1>
+        <h1>textinho</h1>
+    </div>
+}
+
+export default home;
